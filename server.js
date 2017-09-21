062a8d2d714df03a7bd3cbfadecbd592a479fe01
System <none>
2017-09-21 15:52:56 +0000

    basic setup

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.listen(process.env.PORT,);
