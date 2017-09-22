b335da0c8b0b64cf637bd4d6b91d78fff9333243
System <none>
2017-09-22 12:29:25 +0000

    null

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
-var express = require('express');
+const express = require('express');
+const multer = require('multer');
 var app = express();
+//store files as buffer objects
+var storage = multer.memoryStorage();
+var upload = multer({storage: storage});
 app.use(express.static('public'));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
+});
+
+app.post('/upload', upload.single('data'), function(req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify({size: req.file.size}));
 });
 
 app.listen(process.env.PORT,);
